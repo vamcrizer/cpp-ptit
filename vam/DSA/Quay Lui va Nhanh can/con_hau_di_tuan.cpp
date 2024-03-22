1fa693098f72fe8c29a2c6c11bf29621a79fac9c
Vũ Hoàng Anh <142197061+vamcrizer@users.noreply.github.com>
2024-03-22 16:24:32 +0700

    commit

--- vam/DSA/Quay Lui va Nhanh can/con_hau_di_tuan.cpp
+++ vam/DSA/Quay Lui va Nhanh can/con_hau_di_tuan.cpp
@@ -5,9 +5,15 @@ const int MOD = (int)1e9 + 7;
 
 // Cho một bàn cờ vua kích thước NxN, hãy xếp K con hậu trên bàn cờ đó sao cho không có con hậu nào ăn nhau
 int a[100], d1[100], d2[100], cot[100];
-int n;
-
+int X[100][100];
+int n = 8;
+int res = 0;
 void ktao(){
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= n; j++){
+            cin >> X[i][j];
+        }
+    }
     memset(cot, 0, sizeof(cot));
     memset(d1, 0, sizeof(d1));
     memset(d2, 0, sizeof(d2));
@@ -20,10 +26,11 @@ void Try(int i){
             d1[i - j + n] = 1;
             d2[i + j - 1] = 1;
             if (i == n){
+                int sum = 0;
                 for (int k = 1; k <= n; k++){
-                    cout << "Hang: " << k << " " << "Cot: " << a[k] << endl;
+                    sum += X[k][a[k]];
                 }
-                cout << endl;
+                res = max(res, sum);
             }
             Try(i + 1);
             cot[j] = 0;
@@ -34,7 +41,12 @@ void Try(int i){
 }
 
 int main(){
-    cin >> n;
-    ktao();
-    Try(1);
+    int t; cin >> t;
+    while(t--){
+        res = 0;
+        ktao();
+        Try(1);
+        cout << res << endl;
+    }
+    
 }
